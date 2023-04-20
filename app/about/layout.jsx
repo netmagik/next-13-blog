7a46dc1047c0e440c1efcf5dc0c49a9f33f31a93
Irina <irina.blumenfeld@netlify.com>
2023-04-20 13:56:40 -0400

    Update layout text

--- app/about/layout.jsx
+++ app/about/layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutLayout = ({ children }) => {
   return (
     <div>
-      <h1>This is About Layout</h1>
+      <h1>This is a Layout for About and Team</h1>
       {children}
     </div>
   );

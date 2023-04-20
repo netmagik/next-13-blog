541c5c57c18f6b47040c7a56e88920ac01d77d0c
Irina <irina.blumenfeld@netlify.com>
2023-04-20 16:26:07 -0400

    Add Header for cache control

--- netlify/edge-functions/set-cookies/set-cookies.js
+++ netlify/edge-functions/set-cookies/set-cookies.js
@@ -1,5 +1,7 @@
 export default async (request, context) => {
 
+  request.headers.set("cache-control", "public, s-maxage=3600");
+
   console.log(request)
       context.cookies.set({
         name: 'action',
